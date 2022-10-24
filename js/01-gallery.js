36edec6bb6bbf3e29817673e7b4aa232ebd83b56
Yevhen2701 <106544836+EvgenAltunin@users.noreply.github.com>
2022-10-24 23:13:52 +0300

    Gallery was created and added event delegation

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,46 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+// console.log(galleryItems);
+
+const galleryContainer = document.querySelector('.gallery')
+const galleryItemsMurkup = createGalleryItemsMurkup(galleryItems)
+
+galleryContainer.insertAdjacentHTML('beforeend', galleryItemsMurkup)
+
+galleryContainer.addEventListener('click', onGalleryContainerClick)
+
+// console.log(createGalleryItemsMurkup(galleryItems))
+
+function createGalleryItemsMurkup(galleryItems) {
+    return galleryItems.map(({ preview, original, description }) => {
+        return  `
+        <div class="gallery__item">
+            <a class="gallery__link" href="${original}">
+                <img
+                    class="gallery__image"
+                    src="${preview}"
+                    data-source="${original}"
+                    alt="${description}"
+                />
+            </a>
+        </div>
+        `
+    })
+    .join('')
+
+}
+
+function onGalleryContainerClick(event) {
+    // console.log(event.target.nodeName)
+
+    if (event.target.nodeName !== 'IMG') {
+        return;
+    }
+
+    event.preventDefault()
+
+
+    const originalImgUrl = event.target.dataset.source
+    console.log(originalImgUrl)
+}
